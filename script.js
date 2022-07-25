f40bc686bcb18172f9cf88ab51c60e63bb7c17e4
graycodesnu <graysonharvey@graysons-mbp.lan>
2022-07-24 22:57:04 -0500

    created html form container for search bar. created javascript button event listener.

---
+++ script.js
@@ -0,0 +1,14 @@
+// var searchInput = 
+// var searchButton =
+// var currentTemp = 
+// var currentWeatherResults = 
+
+// Search Event Listeners
+searchButton.addEventListener("click") {
+    var searchInput = document.querySelector('.input');
+}
+
+// Display weather results 
+function weatherResults(){
+    
+}
